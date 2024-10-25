104ead91984716e2a70cbf939a2a541754772465
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2024-10-25 15:01:52 +0300

    Api Tarafına UseEffect İle İstek Atıldı

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,37 +1,53 @@
 import TaskCreate from '../public/Components/TasksCreate/TaskCreate'
 import TaskList from '../public/Components/TaskList/TaskList'
-import { useState } from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
   const [task, setTask] = useState([])
 
-  const createTask = (title , taskDesc) => {
-    const createdTask = [
-      ...task,{
-        id : Math.round(Math.random()*9999),
-        title : title,
-        taskDesc : taskDesc,
-      }
-    ];
+  const createTask = async (title , taskDesc) => {
+    const response = await axios.post('http://localhost:3000/task' , {
+      title : title,
+      taskDesc : taskDesc,
+    });
+    const createdTask = [...task,response.data,];
     setTask(createdTask)
   };
+  const fetchTask = async () => {
+    const response = await axios.get('http://localhost:3000/task')
+    setTask(response.data);
+  };
+  useEffect(()=>{
+    fetchTask();
+  },[]);
 
-  const deleteTaskById = (id) => {
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3000/task/${id}`)
     const afterDeletingTask = task.filter((task) => {
       return task.id!== id;
     })
     setTask(afterDeletingTask);
   };
-  const editTaskById = (id , updateTitle ,updateTaskDesc) => {
-    const updatedTask = task.map((task) => {
-      if(task.id === id){
-        return {id : id , title : updateTitle , taskDesc : updateTaskDesc};
-      }
-      return task;
-    })
-    setTask(updatedTask);
+  const editTaskById = async (id , updateTitle ,updateTaskDesc) => {
+    try{
+      await axios.put(`http://localhost:3000/task/${id}` , {
+        title : updateTitle,
+        taskDesc : updateTaskDesc,
+      })
+      const updatedTask = task.map((task) => {
+        if(task.id === id){
+          return {id : id , title : updateTitle , taskDesc : updateTaskDesc};
+        }
+        return task;
+      })
+      setTask(updatedTask);
+    }catch(error){
+      console.error("Görev güncellenirken bir hata oluştu:", error);
+    };
+    
   };
   
   return (
